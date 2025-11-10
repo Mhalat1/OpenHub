fe9d57681de1bff32a122a16eb8778d644e7f620
halat <111527681+Mhalat1@users.noreply.github.com>
2025-11-10 23:25:44 +0100

    Fix route login in security.yaml

--- backend/src/Controller/LoginController.php
+++ backend/src/Controller/LoginController.php
@@ -12,7 +12,7 @@ class LoginController extends AbstractController
     {
         public function __construct(private AuthenticationServiceInterface $authService) {}
 
-        #[Route(path: '/api//login', name: 'app_login', methods: ['POST'])]
+        #[Route(path: '/api/login', name: 'app_login', methods: ['POST'])]
         public function login(): Response
         {
             $error = $this->authService->getLastError();
