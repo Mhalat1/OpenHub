1859f62e7f9b43c7b2ef5256d64202f7af57f05d
halat <111527681+Mhalat1@users.noreply.github.com>
2026-01-29 14:27:35 +0100

    correction index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,8 +4,6 @@ import { BrowserRouter } from 'react-router-dom'; // ✅ ajouter
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { sendFrontendMetrics } from "./metrics";
-sendFrontendMetrics();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
